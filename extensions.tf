0dca8b448e22e55dc9a590df5ca609d7b7531d47
audunsolemdal <audun@solom.no>
2024-02-03 17:11:31 +0100

    avd: make aadJoin optional

--- extensions.tf
+++ extensions.tf
@@ -136,7 +136,7 @@ resource "azurerm_virtual_machine_extension" "avd_register_session_host" {
     "configurationFunction" : "Configuration.ps1\\AddSessionHost",
     "properties" : {
       "hostPoolName" : var.avd_register_session_host["host_pool_name"],
-      "aadJoin" : true
+      "aadJoin" : var.avd_register_session_host["aad_join"],
     }
   })
   protected_settings = jsonencode({
