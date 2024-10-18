1c08cac7f37df184858dabc1dd9552477d809446
audunsolemdal <audun@solom.no>
2024-10-18 15:03:32 +0200

    windows: update aad vm extension to v2

--- extensions.tf
+++ extensions.tf
@@ -9,7 +9,7 @@ resource "azurerm_virtual_machine_extension" "aad_extension_windows" {
   name                       = "AADLoginForWindows"
   publisher                  = "Microsoft.Azure.ActiveDirectory"
   type                       = "AADLoginForWindows"
-  type_handler_version       = "1.0"
+  type_handler_version       = "2.0"
   auto_upgrade_minor_version = true
   virtual_machine_id         = azurerm_windows_virtual_machine.win_vm[0].id
 
